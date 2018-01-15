1c221579e29be83279324d5deaa0875073503a81
Belov Roman <otezzz1982@gmail.com>
2018-01-15 02:28:34 +0200

    Friends filter package.

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,11 @@
  * Third party. For example //= ../../bower_components/jquery/dist/jquery.js
  */
 
+//= ../../bower_components/handlebars/handlebars.min.js
+
 
 /*
  * Custom
  */
+
 //= partials/app.js
