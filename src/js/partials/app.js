b065d01105606ba418314ea368ce93ffac588715
Belov Roman <otezzz1982@gmail.com>
2018-01-16 23:37:37 +0200

    Friends filter package.

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,3 +1,22 @@
-let conLog = 'Галп скомпилил!!!';
+new Promise(function (resolve) {
+    document.addEventListener('DOMContentLoaded', function () {
+        resolve();
+    });
+}).then(function () {
+    return new Promise(function (resolve, reject) {
+        VK.init({
+            apiId:6335009
+        });
 
-console.log(conLog);
+        VK.Auth.login(function(response){
+            if (response.session) {
+                console.log('Все ок!!!');
+                resolve();
+            } else {
+                reject(new Error('Не удалось авторизоваться!!!'));
+            }
+        }, 2 | 4);
+    });
+});
+
+
